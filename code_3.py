9995e2e8271b6e01637bb2049bf40adeeaef6e9a
segabaser <139810447+segabaser@users.noreply.github.com>
2023-12-15 08:42:26 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+function generateRandomData() {
+    });
+main();
+
+    }
+
+        console.log("Random Number: " + item);
+}
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    const data = [];
+    data.forEach(item => {
+function main() {
+}
